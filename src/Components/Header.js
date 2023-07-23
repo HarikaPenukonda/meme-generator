330af0b761b6ea8fc19cbe5898f5f5cdebf1a84c
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-23 13:26:03 -0500

    Create Header Component

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/*
+    Challenge: Build the Header Component
+*/ 
+
+export default function Header(){
+    return(
+        <header>
+            <img/>
+            <h2>Header Component</h2>
+        </header>
+       
+    )
+}
