b4ce80f21f6b6c10fed98c85efd692083038ddd1
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-23 14:06:25 -0500

    adding meme - component and styling form element

---
+++ src/Components/Meme.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Meme(){
+    return(
+        <main>
+            <form className="form">
+            <input 
+                type="text"
+                className="form--input"
+                placeholder="Top text"
+                />
+            <input 
+                type="text"
+                className="form--input" 
+                placeholder="Botton text"   
+                />
+            <button className="form--button">Get a new image</button>
+        </form>
+        </main>
+        
+    )
+}
