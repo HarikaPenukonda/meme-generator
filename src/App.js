4937dc2b112754e01cd95b15405a40b25f6f2846
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-23 14:06:57 -0500

    adding meme component and styling form element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Header from "./Components/Header";
+import Meme from "./Components/Meme";
+
+/*
+    Challenge:
+    - create a Meme component
+    - Inside the Meme Component, render a styled form
+    - with our 2 inputs and the button
+*/
 
 export default function App(){
     return(
         <div>
             <Header/>
+            <Meme/>
         </div>
     )
 }
