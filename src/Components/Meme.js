325ff4098db2f5ccda2c589ddbff9b1a6068f021
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-24 17:01:31 -0500

    get random memes from data

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,6 +1,20 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme(){
+    /*
+        Challenge: Get a random meme from the `memes data` array
+        when the "new meme image button is clicked"
+
+        Log the url of the image to console.
+        
+    */
+   function getMemeImage(){
+    console.log("clicked")
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.random(Math.floor() * memesArray.length)
+    const url = memesArray[randomNumber].url // the url of the meme
+   }
     return(
         <main>
             <form className="form">
@@ -14,7 +28,7 @@ export default function Meme(){
                 className="form--input" 
                 placeholder="Botton text"   
                 />
-            <button className="form--button">Get a new image</button>
+            <button onClick={getMemeImage} className="form--button">Get a new image</button>
         </form>
         </main>
         
