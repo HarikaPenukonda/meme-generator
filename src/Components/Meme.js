013f52ca1b52c99510716b7b8d87fa8bfe22712f
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-24 17:14:24 -0500

    my own code - challenge

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -3,34 +3,40 @@ import memesData from "../memesData";
 
 export default function Meme(){
     /*
-        Challenge: Get a random meme from the `memes data` array
-        when the "new meme image button is clicked"
-
-        Log the url of the image to console.
+        Challenge: Save the random meme URL in state
+            - Create new state called  `memeImage` with an empty string as default
+            - when the getMemeImage function is called, update the `memeImage` state to be the random chosen image URL
+            - Below the div.form, add an <img/> and set the src to the new `memeImage` state you created
         
     */
-   function getMemeImage(){
-    console.log("clicked")
-    const memesArray = memesData.data.memes
-    const randomNumber = Math.random(Math.floor() * memesArray.length)
-    const url = memesArray[randomNumber].url // the url of the meme
-   }
-    return(
-        <main>
-            <form className="form">
-            <input 
-                type="text"
-                className="form--input"
-                placeholder="Top text"
+    let memeImage = React.useState("")
+
+    function getMemeImage(){
+        console.log("clicked")
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.random(Math.floor() * memesArray.length)
+        const url = memesArray[randomNumber].url // the url of the meme
+        memeImage = url
+    }
+        return(
+            <main>
+                <form className="form">
+                <input 
+                    type="text"
+                    className="form--input"
+                    placeholder="Top text"
+                    />
+                <input 
+                    type="text"
+                    className="form--input" 
+                    placeholder="Botton text"   
+                    />
+                <img src={memeImage}
+                    alt="meme"
                 />
-            <input 
-                type="text"
-                className="form--input" 
-                placeholder="Botton text"   
-                />
-            <button onClick={getMemeImage} className="form--button">Get a new image</button>
-        </form>
-        </main>
-        
-    )
+                <button onClick={getMemeImage} className="form--button">Get a new image</button>
+            </form>
+            </main>
+            
+        )
 }
